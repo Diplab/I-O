a31c6c6ee9c0df08dd4d639543a6066259e0cd5c
yingjhu <yingjhu@hotmail.com>
2014-10-21 00:19:01 +0800

    BufferedStreamDemo

---
+++ BufferedStreamDemo/src/BufferedStreamDemo.java
@@ -0,0 +1,37 @@
+import java.io.*;
+ 
+public class BufferedStreamDemo {
+    public static void main(String[] args) {
+        try {
+            byte[] data = new byte[1]; 
+
+            File srcFile = new File("FileDemo.java"); 
+            File desFile = new File("FileDemo.txt"); 
+
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(srcFile)); 
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(desFile));
+ 
+            System.out.println("複製檔案：" + srcFile.length() + "位元組");
+
+            while(bufferedInputStream.read(data) != -1) { 
+                bufferedOutputStream.write(data); 
+            }
+            
+            // 將緩衝區中的資料全部寫出 
+            bufferedOutputStream.flush();
+ 
+            // 關閉串流 
+            bufferedInputStream.close(); 
+            bufferedOutputStream.close(); 
+
+            System.out.println("複製完成"); 
+        } 
+        catch(ArrayIndexOutOfBoundsException e) { 
+            System.out.println("using: java UseFileStream src des"); 
+            e.printStackTrace(); 
+        } 
+        catch(IOException e) { 
+            e.printStackTrace(); 
+        } 
+    }
+} 
