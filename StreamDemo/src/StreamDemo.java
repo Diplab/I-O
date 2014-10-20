c17dfb58683de010cde6dc12aac63350b2f81774
yingjhu <yingjhu@hotmail.com>
2014-10-21 00:18:03 +0800

    StreamDemo

---
+++ StreamDemo/src/StreamDemo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class StreamDemo {
+	 public static void main(String[] args) { 
+	        try { 
+	            System.out.print("輸入字元: "); 
+	            System.out.println("輸入字元十進位表示: " + 
+	                                    System.in.read());  
+	        } 
+	        catch(IOException e) { 
+	            e.printStackTrace(); 
+	        } 
+	    } 
+
+}
