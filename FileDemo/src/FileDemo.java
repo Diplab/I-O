110cac8476b9f3cecdd11b272dd030d87eaff1a3
yingjhu <yingjhu@hotmail.com>
2014-10-21 23:31:42 +0800

    Modification FileDemo.java

--- FileDemo/src/FileDemo.java
+++ FileDemo/src/FileDemo.java
@@ -1,47 +1,30 @@
 import java.io.*;
-import java.util.*;
-
 public class FileDemo {
-    public static void main(String[] args) {
-        try { 
-            File file = new File("test.txt");
-            if(file.isFile()) { // 是否為檔案
-                System.out.println("test.txt" + " 檔案"); 
-                System.out.print(
-                      file.canRead() ? "可讀 " : "不可讀 "); 
-                System.out.print(
-                      file.canWrite() ? "可寫 " : "不可寫 "); 
-                System.out.println(
-                      file.length() + "位元組"); 
-            } 
-            else { 
-                // 列出所有的檔案及目錄
-                File[] files = file.listFiles(); 
-                ArrayList<File> fileList = 
-                                    new ArrayList<File>(); 
-                for(int i = 0; i < files.length; i++) { 
-                    // 先列出目錄 
-                    if(files[i].isDirectory()) { //是否為目錄
-                        // 取得路徑名
-                        System.out.println("[" + 
-                                files[i].getPath() + "]"); 
-                    }
-                    else {
-                        // 檔案先存入fileList，待會再列出
-                        fileList.add(files[i]); 
-                    }
-                } 
 
-                // 列出檔案 
-                for(File f: fileList) {
-                    System.out.println(f.toString());
-                }
-                System.out.println(); 
-            } 
-        } 
-        catch(ArrayIndexOutOfBoundsException e) { 
-            System.out.println(
-                        "using: java FileDemo pathname"); 
-        } 
+    public static void main(String[] args) throws IOException{
+        File dir = new File("txt");
+        File file = new File(dir, "note.txt");
+        if(!dir.exists()){
+            System.out.print(dir.getName() + "目錄是否建立成功?");
+            System.out.println(dir.mkdirs());
+        }
+        else
+            System.out.print(dir.getName() + "目錄已存在");
+
+        if(!file.exists()){
+            System.out.print(file.getName() + "檔案是否建立成功?");
+            System.out.println(file.createNewFile());
+        }
+        else
+            System.out.print(file.getName() + "檔案已存在");
+
+        File newFile = new File(file.getParent(), "comment.txt");
+        System.out.println(file.getName() + "改名為" + newFile.getName());
+        System.out.print("目錄是否修改成功?");
+        System.out.println(file.renameTo(newFile));
+        System.out.print(newFile.getName() + "檔案是否刪除成功?");
+        System.out.println(newFile.delete());
+        System.out.print(dir.getName() + "目錄是否刪除成功?");
+        System.out.println(dir.delete());
     }
 }
